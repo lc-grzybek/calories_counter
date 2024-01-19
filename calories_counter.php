a869f77e04d3bfcadb9deba555882cdc1e64e453
lc-grzybek <kornel.grzywacz@gmail.com>
2024-01-19 23:34:42 +0100

    Create calories_counter.php

---
+++ calories_counter.php
@@ -0,0 +1,50 @@
+<?php
+echo "Calories counter" . PHP_EOL . "Enter number of ingredients" . PHP_EOL;
+$numberOfIngredients = readline(">>");
+if ($numberOfIngredients < 1 || $numberOfIngredients >10)
+{
+    echo "Number of ingredients must be greatar than zero and equal or less than 10" . PHP_EOL;
+}
+else
+{
+    $ingredients = getIngredients($numberOfIngredients);
+    echo "Total caloricity is " . calculateTotalCalories($ingredients) . " kilocalories" . PHP_EOL;
+    foreach ($ingredients as $ingredient)
+    {
+        echo $ingredient['name'] . " " . $ingredient['caloricity'] . " kcal/100g " . ($ingredient['caloricity'] * $ingredient['weight']/100) .  " kcal " . $ingredient['weight'] . " g " . PHP_EOL ;
+    }
+
+}
+
+function getIngredients($numberOfIngredients){
+
+for ($i = 1; $i<=$numberOfIngredients; $i++)
+{
+    echo "Enter $i ingredient name" . PHP_EOL;
+    $ingredientName = (string)readline(">>");    
+    echo "Enter $i ingredient caloricity per 100 g" . PHP_EOL;
+    $caloricity = (int)readline(">>");
+    echo "Enter $i ingredient weight";
+    $weight = (int)readline(">>");
+
+    $ingredients[] = [
+        'name' => $ingredientName,
+        'caloricity' => $caloricity,
+        'weight' => $weight,
+    ];
+}
+return $ingredients;
+}
+function calculateTotalCalories($ingredients)
+    {
+        $totalCalories = 0;        
+        foreach ($ingredients as $ingredient) {            
+            $totalCalories += ($ingredient['caloricity'] * $ingredient['weight']/100);            
+        }
+        return $totalCalories;
+    }
+
+
+
+
+
